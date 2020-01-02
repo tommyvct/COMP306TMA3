eedc1ba56ab5f4eb4786404ad24491875576049a
Tommy Vercetti <7903172+tommyvct@users.noreply.github.com>
2020-01-01 23:14:52 -0800

    started Q3

--- src/test.sh
+++ src/test.sh
@@ -2,45 +2,47 @@
 clang++ TMA3Question1.cpp -o Q1.out
 clang++ TMA3Question2.cpp -o Q2.out
 
-./Q1.out 2>> a.out
-./Q1.out 2>> a.out
-./Q1.out 2>> a.out
-./Q1.out 2>> a.out
-./Q1.out 2>> a.out
-./Q1.out 2>> a.out
-./Q1.out 2>> a.out
-./Q1.out 2>> a.out
-./Q1.out 2>> a.out
-./Q1.out 2>> a.out
-./Q1.out 2>> a.out
-./Q1.out 2>> a.out
-./Q1.out 2>> a.out
-./Q1.out 2>> a.out
-./Q1.out 2>> a.out
-./Q1.out 2>> a.out
-./Q1.out 2>> a.out
-./Q1.out 2>> a.out
-./Q1.out 2>> a.out
-./Q1.out 2>> a.out
+> result
 
-echo "==========================" >> a.out
-./Q2.out 2>> a.out
-./Q2.out 2>> a.out
-./Q2.out 2>> a.out
-./Q2.out 2>> a.out
-./Q2.out 2>> a.out
-./Q2.out 2>> a.out
-./Q2.out 2>> a.out
-./Q2.out 2>> a.out
-./Q2.out 2>> a.out
-./Q2.out 2>> a.out
-./Q2.out 2>> a.out
-./Q2.out 2>> a.out
-./Q2.out 2>> a.out
-./Q2.out 2>> a.out
-./Q2.out 2>> a.out
-./Q2.out 2>> a.out
-./Q2.out 2>> a.out
-./Q2.out 2>> a.out
-./Q2.out 2>> a.out
-./Q2.out 2>> a.out
+./Q1.out 2>> result
+./Q1.out 2>> result
+./Q1.out 2>> result
+./Q1.out 2>> result
+./Q1.out 2>> result
+./Q1.out 2>> result
+./Q1.out 2>> result
+./Q1.out 2>> result
+./Q1.out 2>> result
+./Q1.out 2>> result
+./Q1.out 2>> result
+./Q1.out 2>> result
+./Q1.out 2>> result
+./Q1.out 2>> result
+./Q1.out 2>> result
+./Q1.out 2>> result
+./Q1.out 2>> result
+./Q1.out 2>> result
+./Q1.out 2>> result
+./Q1.out 2>> result
+
+echo "==========================" >> result
+./Q2.out 2>> result
+./Q2.out 2>> result
+./Q2.out 2>> result
+./Q2.out 2>> result
+./Q2.out 2>> result
+./Q2.out 2>> result
+./Q2.out 2>> result
+./Q2.out 2>> result
+./Q2.out 2>> result
+./Q2.out 2>> result
+./Q2.out 2>> result
+./Q2.out 2>> result
+./Q2.out 2>> result
+./Q2.out 2>> result
+./Q2.out 2>> result
+./Q2.out 2>> result
+./Q2.out 2>> result
+./Q2.out 2>> result
+./Q2.out 2>> result
+./Q2.out 2>> result
