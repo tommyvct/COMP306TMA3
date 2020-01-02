2ca02d226ccd39dd9eef9ae332aa7641504cc213
Tommy Vercetti <7903172+tommyvct@users.noreply.github.com>
2020-01-02 01:11:07 -0800

    Q3 half done

--- src/TMA3Question3.cpp
+++ src/TMA3Question3.cpp
@@ -1,8 +1,100 @@
 #include <iostream>
+#include <string>
 
-using namespace std;
+// #define printDoStuff(x) 
+
+class DoStuff
+{
+private:
+    int payload;
+
+public:
+    // constructors
+    DoStuff() : payload(0) {}
+    DoStuff(int newInteger) : payload(newInteger) {}
+    DoStuff(const DoStuff &toCopy) : payload(toCopy.payload) {}
+
+    //unary +
+    DoStuff operator+ () const 
+    {
+        return *this;
+    }  // unary + does nothing!
+
+    // binary +
+    const DoStuff operator+(const DoStuff& rvalue) const
+    {
+        return DoStuff(this -> payload + rvalue.payload);
+    }
+
+    // binary -
+    const DoStuff operator-(const DoStuff& rvalue) const
+    {
+
+        return DoStuff(this -> payload - rvalue.payload);
+    }
+
+    // const DoStuff& operator++ (DoStuff& a) is invalid in c++11!!!!
+    // prefix overloaded operater++ function takes no parameters
+
+    //prefix ++
+    DoStuff& operator++ ()
+    {
+        ++ this ->payload;
+        return *this;
+    }
+
+    //prefix --
+    DoStuff& operator-- ()
+    {
+        -- this -> payload;
+        return *this;
+    }
+
+    //postfix ++
+    DoStuff operator++ (int)
+    {
+        DoStuff ret(*this);
+        this ->payload++;
+        return ret;
+    }
+
+    //postfix --
+    DoStuff operator-- (int)
+    {
+        DoStuff ret(*this);
+        this ->payload--;
+        return ret;
+    }
+
+    void print(std::ostream& out)
+    {
+        out << std::to_string(this -> payload);
+    }
+};
+
 
 int main()
 {
-    
+    DoStuff a(2);
+    DoStuff b(3);
+    DoStuff c(5);
+
+    DoStuff d(a + b - c);
+
+    std::cout << "normal arithmetic" << std::endl;
+    std::cout << "2 + 3 - 5 = " << 2 + 3 - 5 << std::endl;
+    std::cout << std::endl << std::endl;
+
+    std::cout << "DoStuff arithmetic" << std::endl;
+    std::cout << "2 + 3 - 5 = " ;
+    d.print(std::cout);
+    std::cout << std::endl;
+
+    std::cout << "Increment d prefix and postfix: " << std::endl;
+    d++.print(std::cout); // 10
+    d.print(std::cout);  // 11
+    ++d.print(std::cout); // 12
+
+
+    return 0;
 }
