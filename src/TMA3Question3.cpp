fee1048978156e0be4b549bf043652481517f6a6
Tommy Vercetti <7903172+tommyvct@users.noreply.github.com>
2020-01-03 01:05:10 -0800

    finished Q3

--- src/TMA3Question3.cpp
+++ src/TMA3Question3.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <string>
 
-// #define printDoStuff(x) 
 
+// TODO: format to submission standards
+// TODO: impllement a reversed polish evaluator if time permits
 class DoStuff
 {
 private:
@@ -15,17 +16,18 @@ public:
     DoStuff(const DoStuff &toCopy) : payload(toCopy.payload) {}
 
     //unary +
-    DoStuff operator+ () const 
+    const DoStuff operator+ () const 
     {
         return *this;
     }  // unary + does nothing!
+
 
     // binary +
     const DoStuff operator+(const DoStuff& rvalue) const
     {
         return DoStuff(this -> payload + rvalue.payload);
     }
-
+    
     // binary -
     const DoStuff operator-(const DoStuff& rvalue) const
     {
@@ -54,7 +56,7 @@ public:
     DoStuff operator++ (int)
     {
         DoStuff ret(*this);
-        this ->payload++;
+        this -> payload++;
         return ret;
     }
 
@@ -64,6 +66,12 @@ public:
         DoStuff ret(*this);
         this ->payload--;
         return ret;
+    }
+
+    friend std::ostream& operator<<(std::ostream& loperand, 
+                                                        const DoStuff& roperand)
+    {
+        return loperand << std::to_string(roperand.payload);
     }
 
     void print(std::ostream& out)
@@ -77,23 +85,48 @@ int main()
 {
     DoStuff a(2);
     DoStuff b(3);
-    DoStuff c(5);
+    DoStuff c(-5);
 
     DoStuff d(a + b - c);
 
     std::cout << "normal arithmetic" << std::endl;
-    std::cout << "2 + 3 - 5 = " << 2 + 3 - 5 << std::endl;
-    std::cout << std::endl << std::endl;
-
-    std::cout << "DoStuff arithmetic" << std::endl;
-    std::cout << "2 + 3 - 5 = " ;
-    d.print(std::cout);
+    std::cout << "2 + 3 - (-5) = " << 2 + 3 - (-5) << std::endl;
     std::cout << std::endl;
 
+    std::cout << "DoStuff arithmetic" << std::endl;
+    std::cout << "2 + 3 - (-5) = " ;
+    d.print(std::cout);
+    std::cout << std::endl << std::endl;
+
     std::cout << "Increment d prefix and postfix: " << std::endl;
-    d++.print(std::cout); // 10
-    d.print(std::cout);  // 11
-    ++d.print(std::cout); // 12
+    d++.print(std::cout);  // 10
+    std::cout << std::endl;
+    
+    d.print(std::cout);   //  11
+    std::cout << std::endl;
+    
+    (++d).print(std::cout);  // 12
+    std::cout << std::endl;
+    
+    d.print(std::cout);      // 12
+    std::cout << std::endl;
+
+
+    std::cout << std::endl;
+
+
+    d--.print(std::cout);  // 12
+    std::cout << std::endl;
+    
+    d.print(std::cout);   //  11
+    std::cout << std::endl;
+    
+    (--d).print(std::cout);  // 10
+    std::cout << std::endl;
+    
+    // d.print(std::cout);      // 10
+    
+    std::cout << d << std::endl;
 
 
     return 0;
